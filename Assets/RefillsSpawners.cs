fd691e2e02aad0ac0c18d70750f6feac4927d8ca
Kristian G Hammond <77787608+Earth2Kristian@users.noreply.github.com>
2024-04-07 17:17:08 +0100

    Fixed the refillers

--- Assets/RefillsSpawners.cs
+++ Assets/RefillsSpawners.cs
@@ -33,25 +33,25 @@ public class RefillsSpawners : MonoBehaviour
 
         if (pistolRefillTimeSpawn <= 0)
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-22.8f, 22.5f), 1f, Random.Range(-20.8f, 18.9f));
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-22.8f, 22.5f), 1.5f, Random.Range(-20.8f, 18.9f));
             Instantiate(pistolAmmonRefillerObject, randomSpawnPosition, Quaternion.identity);
             pistolRefillTimeSpawn = Random.Range(20, 40);
         }
         if (rifleRefillTimeSpawn <= 0)
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-22.8f, 22.5f), 1f, Random.Range(-20.8f, 18.9f));
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-22.8f, 22.5f), 1.5f, Random.Range(-20.8f, 18.9f));
             Instantiate(rifleAmmonRefillerObject, randomSpawnPosition, Quaternion.identity);
             rifleRefillTimeSpawn = Random.Range(40, 60);
         }
         if (shotgunRefillTimeSpawn <= 0)
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-22.8f, 22.5f), 1f, Random.Range(-20.8f, 18.9f));
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-22.8f, 22.5f), 1.5f, Random.Range(-20.8f, 18.9f));
             Instantiate(shotgunAmmonRefillerObject, randomSpawnPosition, Quaternion.identity);
             shotgunRefillTimeSpawn = Random.Range(60, 80);
         }
         if (healthFillerTimeSpawn <= 0)
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-22.8f, 22.5f), 1f, Random.Range(-20.8f, 18.9f));
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-22.8f, 22.5f), 2f, Random.Range(-20.8f, 18.9f));
             Instantiate(healthFillerObject, randomSpawnPosition, Quaternion.Euler(-90, 0, 0));
             healthFillerTimeSpawn = Random.Range(30, 45);
         }
