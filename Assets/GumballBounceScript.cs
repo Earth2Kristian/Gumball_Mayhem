00d8115c2f64c8198cf360867c93378ed648fa47
Kristian G Hammond <77787608+Earth2Kristian@users.noreply.github.com>
2024-06-09 18:02:15 +0100

    Gumballs are now bouncing

---
+++ Assets/GumballBounceScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class GumballBounceScript : MonoBehaviour
+{
+    Vector3 lastVelocity;
+    public float bounceSpeed = 5f;
+    
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        lastVelocity = rb.velocity;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+       Vector3 normal = collision.contacts[0].normal;
+       Vector3 reflectedVelocity = Vector3.Reflect(lastVelocity.normalized, normal);
+       rb.velocity = reflectedVelocity * bounceSpeed;
+    }
+}
