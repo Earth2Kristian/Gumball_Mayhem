ed4e16b8ccb87e8fe48a6ea1bece27812727ddb8
Kristian G Hammond <77787608+Earth2Kristian@users.noreply.github.com>
2024-08-21 22:19:34 +0100

    Fixing the Quality Settings

--- Assets/GraphicsSettingsScript.cs
+++ Assets/GraphicsSettingsScript.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GraphicsSettingsScript : MonoBehaviour
 {
+    public TMP_Dropdown qualitySettingDropdown;
+
+    private void Start()
+    {
+        qualitySettingDropdown.value = PlayerPrefs.GetInt("QualityLevel", 2);
+    }
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("QualityLevel", qualityIndex);
     }
 }
